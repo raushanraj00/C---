0fa198cf7ed4a4f634719a08eaeca5e2c39b8b9d
raushanraj00 <raushanraj00650@gmail.com>
2026-02-23 14:04:24 +0530

    update

---
+++ SORTING AND SEARCHING/12_bubble_sort_ascending.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std; 
+int main(){
+    int n ; 
+    cout<< "Enter size :- " ; cin>> n; 
+    int arr[1000]; 
+    for(int i=0; i<n ; i++){
+        cout<< "Enter Element " << i+1 << ": "; 
+        cin>> arr[i]; 
+    }
+    for(int i = n-2; i>=0; i--){
+        bool swapped = 1; 
+        for(int j = 0 ; j<=i; j++ ){
+            if(arr[j]> arr[j+1]){
+                swapped = 1; 
+                swap(arr[j],arr[j+1]);
+            }
+        }
+        if(swapped=0)
+        break;   
+    }
+    for(int i = 0; i<n; i++){
+        cout<< arr[i] << "\t";
+    }
+}
+// bubble sort in ascending order
