33ed996a5083e2e8edc21e48f98547b0944729c0
raushanraj00 <raushanraj00650@gmail.com>
2026-02-24 12:09:34 +0530

    Summary

---
+++ Class_and_Objects/4_outside_class.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std;
+class Book{
+    public: 
+    void myname();
+};
+void Book::myname(){
+    cout<< "Hello World!" << endl;
+}
+
+int main(){
+    Book mybook; 
+    mybook.myname(); 
+    return 0; 
+}
