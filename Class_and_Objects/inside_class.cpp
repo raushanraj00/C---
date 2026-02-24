191c36da57c3c8c367ee521966a882f0cd9342aa
raushanraj00 <raushanraj00650@gmail.com>
2026-02-24 12:02:01 +0530

    Create inside_class.cpp

---
+++ Class_and_Objects/inside_class.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std; 
+class Book{
+    public: 
+    void myname(){
+        cout<< "Hello Rohit";
+    }
+    
+};
+int main(){
+    Book mybook; 
+    mybook.myname(); 
+    return 0; 
+}
