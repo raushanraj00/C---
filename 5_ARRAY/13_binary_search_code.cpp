6a0dde647f6d4950c12afbb36287341ea1934c5a
raushanraj00 <raushanraj00650@gmail.com>
2026-02-28 01:01:06 +0530

    Update 13_binary_search_code.cpp

--- 5_ARRAY/13_binary_search_code.cpp
+++ 5_ARRAY/13_binary_search_code.cpp
@@ -31,3 +31,56 @@ int main(){
     if(arr[mid]!=x) cout<< "Search Failed.";
     
 }
+
+// using function
+// #include <iostream> 
+// using namespace std; 
+// int binarysearch(int arr[], int n, int key){
+//     int i =0, j = n-1; 
+//     int mid;
+    
+//     while(i<=j){
+//         // mid element; 
+//          mid = (i+j)/2; 
+        
+//         // check element:- 
+//         if(arr[mid]==key){
+//             return mid; 
+//         }
+//         // arr[mid]<key
+//         else if(arr[mid]<key){
+//             i = mid+1; 
+//         }
+//         // arr[mid]>key; 
+//         else
+//         j = mid-1; 
+        
+//     }
+//     return -1; 
+// }
+// int main(){
+//     int n,key; 
+//     cout<< "Enter no. of elements:-"; 
+//     cin>> n; 
+    
+//     int arr[100]; 
+//     cout<< "Enter array Elements:- "; 
+//     for(int i =0; i<n; i++){
+//         cin>> arr[i]; 
+//     }
+//     cout<< "Enter element to search:- "; 
+//     cin>> key;
+    
+//     binarysearch(arr,n,key); 
+    
+//     int result = binarysearch(arr,n,key); 
+    
+//     if(result!= -1) {
+//         cout<<"Element Found at Index" << result <<endl;
+//     }
+//     else 
+//     {cout<< "Element not Found"; 
+//         }
+
+//     return 0; 
+// }
