bb2d839feab4db83b66dfcab6ba4e988da81bec1
raushanraj00 <raushanraj00650@gmail.com>
2026-03-07 12:29:56 +0530

    update

--- 5_ARRAY/26_peak_index_in_mountain.cpp
+++ 5_ARRAY/26_peak_index_in_mountain.cpp
@@ -1,29 +1,34 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
+
 int main(){
-    int n,mid ; 
-    cout<< "Enter size :- "; 
-    cin>> n; 
-    int arr[100]; 
-    cout<< "Enter Elements :- "<< endl;
-    for(int i =0 ; i<n; i++){
-        cin>> arr[i];
+    int n, mid;
+    cout << "Enter size :- ";
+    cin >> n;
+
+    int arr[100];
+    cout << "Enter Elements :- " << endl;
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
     }
-    int start = 0, end = n-1; 
-    while(start<end){
-        mid = (start+end)/2;
+
+    int start = 0, end = n - 1;
+
+    while(start <= end){
+        mid = (start + end) / 2;
 
         if((mid == 0 || arr[mid] > arr[mid-1]) && (mid == n-1 || arr[mid] > arr[mid+1])){
-            cout<< "Peak Index is :- " << mid << endl; 
+            cout << "Peak Index is :- " << mid << endl;
             break;
         }
-        else if(arr[mid]>arr[mid-1]){
-            start = mid+1; 
+        else if(mid > 0 && arr[mid-1] > arr[mid]){
+            end = mid - 1;
         }
-        else {
-            end = mid-1; 
+        else{
+            start = mid + 1;
         }
     }
 
-    return 0; 
+    return 0;
 }
