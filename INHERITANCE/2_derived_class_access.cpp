fb4b7bac9808d5c6b0ee9b8d3e349b4ddb83fc5a
raushanraj00 <raushanraj00650@gmail.com>
2026-03-08 12:54:50 +0530

    update

--- INHERITANCE/2_derived_class_access.cpp
+++ INHERITANCE/2_derived_class_access.cpp
@@ -2,13 +2,24 @@
 using namespace std; 
 
 class Human{
+    string religion,color; 
     public:     
     string name;
     int age, weight; 
 }; 
-class Student : public Human {
-    public:
+class Student : private Human {
+    private:
         int roll; int fees; 
+    public:
+    void display(string name, int age, int weight, int roll, int fees){
+        this->name = name; 
+        this-> age  = age; 
+        this-> weight = weight; 
+        this-> roll = roll ;
+        this-> fees = fees; 
+        cout<< name << endl << age << endl << weight << endl  << roll << endl << fees << endl ;
+    }
+    
     
 }; 
 class Teacher : private Human{  // private inheritance and protected inheritance are same in terms of access specifier but different in terms of inheritance
@@ -24,12 +35,14 @@ class Teacher : private Human{  // private inheritance and protected inheritance
 };
 
 int main(){
-    // Student s1; 
+    Student s1; 
     // s1.name = "Raushan"; 
     // s1.roll = 123;
     // cout<< "Name :- " << s1.name << endl ; 
     // cout<< "Roll :- " << s1.roll << endl; 
+    s1.display("Rohit",22,50,101,10000); 
     Teacher t1; 
     t1.fun("Raushan", 22, 70);
+    
     return 0; 
 }
