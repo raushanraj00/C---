b24a843cb6134bfcb8f465a53e8688f2d784b5af
raushanraj00 <raushanraj00650@gmail.com>
2026-03-08 12:47:27 +0530

    edrived class

--- INHERITANCE/2_derived_class_access.cpp
+++ INHERITANCE/2_derived_class_access.cpp
@@ -9,12 +9,27 @@ class Human{
 class Student : public Human {
     public:
         int roll; int fees; 
+    
 }; 
+class Teacher : private Human{  // private inheritance and protected inheritance are same in terms of access specifier but different in terms of inheritance
+    public: 
+    void fun(string n, int a, int w){
+        name = n;
+        age = a;
+        weight = w;
+        cout<< "Name :- " << name << endl; 
+        cout<< "Age :- " << age << endl;    
+        cout<< "Weight :- " << weight << endl;
+    }
+};
 
 int main(){
-    Student s1; 
-    s1.name = "Raushan"; 
-    s1.roll = 123;
-    cout<< "Name :- " << s1.name << endl ; 
-    cout<< "Roll :- " << s1.roll << endl; 
+    // Student s1; 
+    // s1.name = "Raushan"; 
+    // s1.roll = 123;
+    // cout<< "Name :- " << s1.name << endl ; 
+    // cout<< "Roll :- " << s1.roll << endl; 
+    Teacher t1; 
+    t1.fun("Raushan", 22, 70);
+    return 0; 
 }
