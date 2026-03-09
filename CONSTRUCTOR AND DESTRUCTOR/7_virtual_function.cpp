abebda2b428e8d930c47c0a3acc99b822967bf14
raushanraj00 <raushanraj00650@gmail.com>
2026-03-10 01:05:05 +0530

    virtual fcn

---
+++ CONSTRUCTOR AND DESTRUCTOR/7_virtual_function.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+using namespace std; 
+class base{
+    public: 
+    void show(){
+        cout<< "This is a base class." << endl; 
+    }
+}; 
+class derived:public base{
+    public: 
+    void show(){
+        cout<< "This is a derived class." << endl; 
+    }
+}; 
+int main(){
+    derived d1; 
+    base *ptr; 
+    ptr = &d1; 
+    ptr -> show(); 
+    
+}
