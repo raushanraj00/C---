c93cf3a28aa2fa7a623d7fb6d9d2387d7b6e4add
raushanraj00 <raushanraj00650@gmail.com>
2026-03-10 01:07:55 +0530

    update

--- CONSTRUCTOR AND DESTRUCTOR/7_virtual_function.cpp
+++ CONSTRUCTOR AND DESTRUCTOR/7_virtual_function.cpp
@@ -2,7 +2,7 @@
 using namespace std; 
 class base{
     public: 
-    void show(){
+    virtual void show(){  // now after writing virtual keyword here , it will call the function of derived class instead of base class because of late binding and also we can call the function of base class using scope resolution operator
         cout<< "This is a base class." << endl; 
     }
 }; 
