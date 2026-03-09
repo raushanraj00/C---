dba6d08a1d0fe61526fb408ffde093baeef74900
raushanraj00 <raushanraj00650@gmail.com>
2026-03-10 00:55:29 +0530

    update

--- CONSTRUCTOR AND DESTRUCTOR/5_operator_overloading.cpp
+++ CONSTRUCTOR AND DESTRUCTOR/5_operator_overloading.cpp
@@ -19,9 +19,16 @@ class data{
         num1 = -num1; 
         return num1;
     }
+    
     void display(){
         cout<< "Value = " << num1 << endl; 
     }
+    // for binary operator overloading 
+    data operator+(data d5){
+        data temp(0); 
+        temp.num1 = num1 + d5.num1; 
+        return temp;
+    }; 
 }; 
 int main(){
     data t1(10),t2(10),t3(10), result(0),resultt(0), resulttt(0);
@@ -31,4 +38,9 @@ int main(){
     resultt.display(); 
     resulttt = -t3; 
     resulttt.display();
+    // for binary operator overloading 
+    // result = t1 + t2;
+    data t4(5),t5(10), resultbinary(0);
+    resultbinary = t4 + t5; 
+    resultbinary.display();
 }
