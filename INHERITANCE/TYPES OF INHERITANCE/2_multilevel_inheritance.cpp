78ed0a2f496b8b5d5c4721b3171515369f9856e3
raushanraj00 <raushanraj00650@gmail.com>
2026-03-09 23:05:44 +0530

    update

--- INHERITANCE/TYPES OF INHERITANCE/2_multilevel_inheritance.cpp
+++ INHERITANCE/TYPES OF INHERITANCE/2_multilevel_inheritance.cpp
@@ -4,9 +4,25 @@ using namespace std;
 class Livingbeing{  // grandparent 
     public:
     void breathe(){ 
-       cout<<  "I am breathing"; 
+       cout<<  "I am breathing"<< endl; 
     }
 };
 class Animal : public Livingbeing{
-    
+    public: 
+    void eat(){
+        cout<< "I am eating." << endl; 
+    }
 };
+class Dog:public Animal{
+    public: 
+    void bark(){
+        cout<< "I am barking. " << endl; 
+    }
+}; 
+int main(){
+    Dog d; 
+    d.breathe(); 
+    d.eat(); 
+    d.bark(); 
+    return 0; 
+}
