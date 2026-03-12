8da5530f0bd78c677d3762c48aa9f586ded5f1ea
raushanraj00 <raushanraj00650@gmail.com>
2026-03-12 23:25:47 +0530

    update

--- LINKED LIST/Singly Linked List/1_basic_node_creation.cpp
+++ LINKED LIST/Singly Linked List/1_basic_node_creation.cpp
@@ -18,29 +18,9 @@ void inserthead(node* &head, int d){
     head = temp; 
 
 }
-
-void printnode(node* head){
-    node* temp = head; 
-    while(temp!=NULL){
-        cout << temp-> data << " "; 
-        temp = temp-> next; 
-    }
-    cout<< endl; 
-}
 int main(){
     // created a new node 
     node* node1 = new node(10); 
     cout<< node1 -> data << endl; 
     cout<< node1 -> next << endl;
-    
-    // head pointed to node1
-    node* head = node1; 
-    cout<< "List before Insertion:- "<< endl; 
-    printnode(head); 
-    inserthead(head,12); 
-    cout<< "List after Insertion:- "<<endl; 
-    printnode(head); 
-    inserthead(head,15); 
-    cout<< "List after Insertion:- "<<endl;
-    printnode(head);
 }
