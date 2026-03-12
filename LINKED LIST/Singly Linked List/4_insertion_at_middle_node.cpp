451370fd4c97be59a2088871f8c991165c9b4ddf
raushanraj00 <raushanraj00650@gmail.com>
2026-03-13 00:04:02 +0530

    at middle

---
+++ LINKED LIST/Singly Linked List/4_insertion_at_middle_node.cpp
@@ -0,0 +1,48 @@
+#include <iostream> 
+using namespace std;
+class node{
+    public : 
+    int data; 
+    node* next; 
+
+    // constructor
+    node(int data){
+        this -> data = data; 
+        this -> next = NULL; 
+    }
+}; 
+void insertatmid(node* &head, int d, int pos){
+    node* temp = head; 
+    int count = 1; 
+
+    while(count< pos-1){
+        temp = temp-> next; 
+        count++ ; 
+    }
+    node* nodetoinsert = new node(d);
+    nodetoinsert-> next = temp -> next; 
+    temp -> next = nodetoinsert;
+}
+void display(node* head){
+    node* temp = head; 
+
+    while(temp!=NULL){
+        cout<< temp-> data << " "; 
+        temp = temp -> next;  
+    }
+    cout<< endl; 
+}
+int main(){
+    node *node1 = new node(10); 
+    node* head = node1; 
+    node* node2 = new node(20); 
+    node1 -> next = node2; 
+    node* node3 = new node(40); 
+    node2 -> next = node3; 
+
+    cout<< "list before insertion:- "<< endl;
+    display(head); 
+    insertatmid(head,30,3);
+    cout<< "List after insertion:- " << endl; 
+    display(head);  
+}
