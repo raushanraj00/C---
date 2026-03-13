ca61732d3fa565bb5461a96ec7d86d114f8ebe79
raushanraj00 <raushanraj00650@gmail.com>
2026-03-13 14:29:10 +0530

    Update 1_basic_node_creation.cpp

--- LINKED LIST/Singly Linked List/1_basic_node_creation.cpp
+++ LINKED LIST/Singly Linked List/1_basic_node_creation.cpp
@@ -1,31 +1,3 @@
-// #include <iostream> 
-// using namespace std; 
-// class node{
-//     public: 
-//     int data; 
-//     node* next;
-    
-//     // constructor 
-//     node(int data){
-//         this-> data = data; 
-//         this-> next = NULL; 
-//     }
-// }; 
-// void inserthead(node* &head, int d){
-//     // new node create 
-//     node* temp = new node(d); 
-//     temp -> next = head; 
-//     head = temp; 
-
-// }
-// int main(){
-//     // created a new node 
-//     node* node1 = new node(10); 
-//     cout<< node1 -> data << endl; 
-//     cout<< node1 -> next << endl;
-// }
-
-
 // without functiomn 
 #include <iostream> 
 using namespace std; 
@@ -58,3 +30,34 @@ int main(){
         
     }
 }
+
+
+
+// #include <iostream> 
+// using namespace std; 
+// class node{
+//     public: 
+//     int data; 
+//     node* next;
+    
+//     // constructor 
+//     node(int data){
+//         this-> data = data; 
+//         this-> next = NULL; 
+//     }
+// }; 
+// void inserthead(node* &head, int d){
+//     // new node create 
+//     node* temp = new node(d); 
+//     temp -> next = head; 
+//     head = temp; 
+
+// }
+// int main(){
+//     // created a new node 
+//     node* node1 = new node(10); 
+//     cout<< node1 -> data << endl; 
+//     cout<< node1 -> next << endl;
+// }
+
+
