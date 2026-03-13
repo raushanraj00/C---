4b5642216502089f8d7c7c897c818025d58ffaca
raushanraj00 <raushanraj00650@gmail.com>
2026-03-13 17:48:57 +0530

    summary

--- LINKED LIST/Singly Linked List/3_insertion_at_tail.cpp
+++ LINKED LIST/Singly Linked List/3_insertion_at_tail.cpp
@@ -1,46 +1,40 @@
-// to insert at a position without using any function
+// insertion at tail of ll 
 #include <iostream> 
 using namespace std; 
 class node{
-    public: 
-    int data ; 
-    node* next; 
-    node(int data){
-        this-> data = data ; 
-        this-> next = NULL; 
-    }
+  public: 
+  int data ; 
+  node* next; 
+   
+  node(int data){
+      this-> data = data ; 
+      this -> next = NULL; 
+  }
 }; 
 int main(){
-    node* node1 = new node(10);
-    node* node2 = new node(20);
-    node* node3 = new node(30); 
-    node* node4 = new node(40); 
-    
-    node1-> next = node2; 
-    node2 -> next = node3; 
-    node3 -> next  = node4; 
-    node4 -> next = NULL; 
+    node* node1 = new node(10); 
+    node1-> next = NULL; 
     
     node* head = node1; 
-    
-    
-    int pos = 3; 
-    node* newnode = new node(25); 
-    
+    // creating new node first of the tail element 
+    node* newnode = new node(20); 
+    // traversing till last element :- 
     node* temp = head; 
-    for(int i = 1; i<pos-1; i++){
-        temp = temp-> next; 
+    while(temp-> next!=NULL){
+         temp = temp-> next;  
     }
-    newnode-> next = temp-> next; 
-    temp-> next = newnode; 
     
+    // inserting
+    temp-> next = newnode; 
     temp = head; 
     
+    // to display final ll 
     while(temp!=NULL){
-        cout<< temp-> data << " "; 
+        cout<< temp-> data <<  " "; 
         temp = temp-> next; 
     }
 }
+
 
 
 
