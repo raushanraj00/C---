36d49756e293f5e055d3631544654fe0c7163783
raushanraj00 <raushanraj00650@gmail.com>
2026-03-13 17:46:46 +0530

    Update 3_insertion_at_tail.cpp

--- LINKED LIST/Singly Linked List/3_insertion_at_tail.cpp
+++ LINKED LIST/Singly Linked List/3_insertion_at_tail.cpp
@@ -1,43 +1,90 @@
+// to insert at a position without using any function
 #include <iostream> 
 using namespace std; 
-class Node{
+class node{
     public: 
     int data ; 
-    Node* next; 
-
-    /// constructor 
-    Node(int data){
-        this -> data = data; 
-        this -> next = NULL; 
+    node* next; 
+    node(int data){
+        this-> data = data ; 
+        this-> next = NULL; 
     }
 }; 
-void inserttail(Node* &tail, int d){
-    // new node create 
-    Node* temp = new Node(d);
-    tail -> next = temp; 
-    tail  = temp; 
-}
-void display(Node* head){
-    Node* temp = head; 
+int main(){
+    node* node1 = new node(10);
+    node* node2 = new node(20);
+    node* node3 = new node(30); 
+    node* node4 = new node(40); 
+    
+    node1-> next = node2; 
+    node2 -> next = node3; 
+    node3 -> next  = node4; 
+    node4 -> next = NULL; 
+    
+    node* head = node1; 
+    
+    
+    int pos = 3; 
+    node* newnode = new node(25); 
+    
+    node* temp = head; 
+    for(int i = 1; i<pos-1; i++){
+        temp = temp-> next; 
+    }
+    newnode-> next = temp-> next; 
+    temp-> next = newnode; 
+    
+    temp = head; 
     
     while(temp!=NULL){
         cout<< temp-> data << " "; 
-        temp = temp -> next; 
+        temp = temp-> next; 
     }
-    cout << endl; 
 }
 
-int main(){
-    Node* node1 = new Node(10); 
 
-    Node* head = node1; 
-    Node* tail = node1; 
 
-    inserttail(tail,20);
-    display(head);  
-    inserttail(tail,30); 
-    display(head); 
-    inserttail(tail,40); 
-    display(head);
-    return 0;    
-}   
+
+// #include <iostream> 
+// using namespace std; 
+// class Node{
+//     public: 
+//     int data ; 
+//     Node* next; 
+
+//     /// constructor 
+//     Node(int data){
+//         this -> data = data; 
+//         this -> next = NULL; 
+//     }
+// }; 
+// void inserttail(Node* &tail, int d){
+//     // new node create 
+//     Node* temp = new Node(d);
+//     tail -> next = temp; 
+//     tail  = temp; 
+// }
+// void display(Node* head){
+//     Node* temp = head; 
+    
+//     while(temp!=NULL){
+//         cout<< temp-> data << " "; 
+//         temp = temp -> next; 
+//     }
+//     cout << endl; 
+// }
+
+// int main(){
+//     Node* node1 = new Node(10); 
+
+//     Node* head = node1; 
+//     Node* tail = node1; 
+
+//     inserttail(tail,20);
+//     display(head);  
+//     inserttail(tail,30); 
+//     display(head); 
+//     inserttail(tail,40); 
+//     display(head);
+//     return 0;    
+// }   
